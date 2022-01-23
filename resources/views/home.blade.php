7122199ef8c1d4b0eba3ad551d0fcf88b82cbc83
kico0304 <kico_jajcanin@hotmail.com>
2022-01-23 22:28:00 +0100

    update slider teksta

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -102,12 +102,11 @@
                 <div class="col-lg-6 col-md-12 col-xl-7">
                     <div class="block">
                         <div class="divider mb-3"></div>
-                        <span class="text-uppercase text-sm letter-spacing ">Total Health care solution</span>
-                        <h1 class="mb-3 mt-3">Your most trusted health partner</h1>
-                        
-                        <p class="mb-4 pr-5">A repudiandae ipsam labore ipsa voluptatum quidem quae laudantium quisquam aperiam maiores sunt fugit, deserunt rem suscipit placeat.</p>
+                        <span class="text-uppercase text-sm letter-spacing ">Mobilne kućice</span>
+                        <h1 class="mb-3 mt-3">Modul Mobile Homes</h1>
+                        <p class="mb-4 pr-5">Ovdje neki dodatni tekst.</p>
                         <div class="btn-container ">
-                            <a href="appoinment.html" target="_blank" class="btn btn-main-2 btn-icon btn-round-full">Make appoinment <i class="icofont-simple-right ml-2  "></i></a>
+                            <a href="appoinment.html" target="_blank" class="btn btn-main-2 btn-icon btn-round-full">Kontaktirajte nas <i class="icofont-simple-right ml-2  "></i></a>
                         </div>
                     </div>
                 </div>
