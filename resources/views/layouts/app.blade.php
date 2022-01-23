682d5c93771864d54ca24786c91b925ac59744cc
kico0304 <kico_jajcanin@hotmail.com>
2022-01-23 21:47:06 +0100

    template implementation stage 1.1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,9 +39,9 @@
     </head>
     <body id="top">
         <div id="app">
-            <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <!-- <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
                 @yield('sidebar')
-            </nav>
+            </nav> -->
 
             <main>
                 @yield('content')
