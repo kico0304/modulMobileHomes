1c3eb867069f0083f27bf6dda905eee321f92bb4
kico0304 <kico_jajcanin@hotmail.com>
2022-02-20 00:52:59 +0100

    fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,7 +19,22 @@
 @section('content')
 
     <!-- TEMPLATE PART START -->
-
+{{--@foreach($products as $product)--}}
+{{--    <p>Product name: {{$product->names[0]->name}}</p>--}}
+{{--    <p>Product price: {{$product->price}}</p>--}}
+{{--    <p>Product surface: {{$product->surface}}</p>--}}
+{{--    <p>Product text: {{$product->texts[0]->text}}</p>--}}
+{{--    @foreach($product->images as $image)--}}
+{{--        <p>Product image name: {{$image->name}}</p>--}}
+{{--    @endforeach--}}
+{{--    @foreach($product->product_parts as $part)--}}
+{{--        <p>Product part name: {{$part->part_names[0]->name}}</p>--}}
+{{--        <p>Product part text: {{$part->part_texts[0]->text}}</p>--}}
+{{--        @foreach($part->part_images as $part_img)--}}
+{{--            <p>Product part images: {{$part_img->name}}</p>--}}
+{{--        @endforeach--}}
+{{--    @endforeach--}}
+{{--@endforeach--}}
     <header>
         <div class="header-top-bar">
             <div class="container">
@@ -43,14 +58,13 @@
         </div>
         <nav class="navbar navbar-expand-lg navigation" id="navbar">
             <div class="container">
-
                 <a class="navbar-brand" href="index.html">
                     <img src="images/logo.png" alt="" class="img-fluid">
                 </a>
 
                 <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarmain" aria-controls="navbarmain" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="icofont-navigation-menu"></span>
-                </button>
+                <span class="icofont-navigation-menu"></span>
+            </button>
 
             <div class="collapse navbar-collapse" id="navbarmain">
                 <ul class="navbar-nav ml-auto">
@@ -59,7 +73,7 @@
                     </li>
 
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="department.html" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Katalog + <i class="icofont-thin-down"></i></a>
+                    <a class="nav-link dropdown-toggle" href="department.html" id="dropdown02" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Katalog + <i class="icofont-thin-down"></i></a>
                         <ul class="dropdown-menu" aria-labelledby="dropdown02">
                             <li><a class="dropdown-item" href="#">MMH21.S01 – kuća za 2 osobe</a></li>
                             <li><a class="dropdown-item" href="#">MMH21.M01 – kuća za 4 osobe</a></li>
@@ -70,7 +84,7 @@
                             <li><a class="dropdown-item" href="#">NAPRAVI SVOJU KOMBINACIJU</a></li>
                         </ul>
                     </li>
-                    
+
                     <li class="nav-item">
                         <a class="nav-link" href="#">Za investitore</a>
                     </li>
@@ -97,24 +111,22 @@
     </header>
 
     <!-- Slider Start -->
-    <section class="banner" active-slide=1 active-text=1>
-        <div id="arrow-left">&#171;</div>
+    <section class="banner">
         <div class="container">
             <div class="row">
                 <div class="col-lg-6 col-md-12 col-xl-7">
                     <div class="block">
                         <div class="divider mb-3"></div>
-                        <span id="smallText" class="text-uppercase text-sm letter-spacing animate">Zašto MMH?</span>
-                        <h1 id="mainTitle" class="mb-3 mt-3 animate">ModulMobileHomes</h1>
-                        <p id="descText" class="mb-4 pr-5 animate">Zato što su naši objekti dimenziosani prema vašim stvarnim potrebama, zato što jednostavnost gradnje štedi vaše vreme, zato šte se moderan dizajn uklapa u svako prirodno okruženje.</p>
+                        <span class="text-uppercase text-sm letter-spacing ">Zašto MMH?</span>
+                        <h1 class="mb-3 mt-3">ModulMobileHomes</h1>
+                        <p class="mb-4 pr-5">Zato što su naši objekti dimenziosani prema vašim stvarnim potrebama, zato što jednostavnost gradnje štedi vaše vreme, zato šte se moderan dizajn uklapa u svako prirodno okruženje.</p>
                         <div class="btn-container">
-                            <a id="buttonText" href="appoinment.html" target="_blank" class="btn btn-main-2 btn-icon btn-round-full animate">Kontaktirajte nas <i class="icofont-simple-right ml-2  "></i></a>
+                            <a href="appoinment.html" target="_blank" class="btn btn-main-2 btn-icon btn-round-full">Kontaktirajte nas <i class="icofont-simple-right ml-2  "></i></a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div id="arrow-right">&#187;</div>
     </section>
 
     <!-- Features Start -->
@@ -132,7 +144,7 @@
                             <p class="mb-4">Informišite se o našim proizvodima svakim radnim danom.</p>
                             <a href="appoinment.html" class="btn btn-main btn-round-full">Zakažite termin</a>
                         </div>
-                    
+
                         <div class="feature-item mb-5 mb-lg-0">
                             <div class="feature-icon mb-4">
                                 <i class="icofont-ui-clock"></i>
@@ -145,7 +157,7 @@
                                 <li class="d-flex justify-content-between">Nedjelja: <span>Ne radimo</span></li>
                             </ul>
                         </div>
-                    
+
                         <div class="feature-item mb-5 mb-lg-0">
                             <div class="feature-icon mb-4">
                                 <i class="icofont-support"></i>
@@ -263,7 +275,7 @@
                     </div>
                 </div>
             </div>
-            
+
             <div class="footer-btm py-4 mt-5">
                 <div class="row align-items-center justify-content-between">
                     <div class="col-lg-7">
@@ -310,183 +322,10 @@
     <!-- STOLETOV DIO ZA TESTIRANJE END -->
 @endsection
 
-<!-- BROJAC SLIKA ZA SLIDER START -->
-<?php 
-$directory = "images/slider/";
-$filecount = 0;
-$files = glob($directory . "*");
-if ($files){$filecount = count($files);}
-?>
-<!-- BROJAC SLIKA ZA SLIDER END -->
-
 @section('js')
-    
-    <script src="{{ asset('js/slider.js') }}"></script>
-
-    <script type="text/javascript">
-
-        //get number of texts for slider
-        var numberOfSliderTexts = Object.keys(slideTexts).length;
-
-        function textSwitch(atn, side, ewa){
-            if(side == "right"){
-                if(atn == numberOfSliderTexts){
-
-                    //smallText
-                    $("#smallText").animate({'opacity': 0}, 400, function(){
-                        $("#smallText").html(slideTexts[1].smallText).animate({'opacity': 1}, 400);    
-                    });
-
-                    //mainTitle
-                    $("#mainTitle").animate({'opacity': 0}, 400, function(){
-                        $("#mainTitle").html(slideTexts[1].mainTitle).animate({'opacity': 1}, 400);    
-                    });
-
-                    //descText
-                    $("#descText").animate({'opacity': 0}, 400, function(){
-                        $("#descText").html(slideTexts[1].descText).animate({'opacity': 1}, 400);    
-                    });
-
-                    //buttonText
-                    $("#buttonText").animate({'opacity': 0}, 400, function(){
-                        $("#buttonText").html(slideTexts[1].buttonText+'<i class="icofont-simple-right ml-2  "></i>').animate({'opacity': 1}, 400);    
-                    });
-
-                    //set new text attribute
-                    ewa.attr("active-text", 1);
-                }else{
-                    var numberNeeded = parseInt(atn) + 1;
-
-                    //smallText
-                    $("#smallText").animate({'opacity': 0}, 400, function(){
-                        $(this).html(slideTexts[numberNeeded].smallText).animate({'opacity': 1}, 400);    
-                    });
-
-                    //mainTitle
-                    $("#mainTitle").animate({'opacity': 0}, 400, function(){
-                        $("#mainTitle").html(slideTexts[numberNeeded].mainTitle).animate({'opacity': 1}, 400);    
-                    });
-
-                    //descText
-                    $("#descText").animate({'opacity': 0}, 400, function(){
-                        $("#descText").html(slideTexts[numberNeeded].descText).animate({'opacity': 1}, 400);    
-                    });
-
-                    //buttonText
-                    $("#buttonText").animate({'opacity': 0}, 400, function(){
-                        $("#buttonText").html(slideTexts[numberNeeded].buttonText+'<i class="icofont-simple-right ml-2  "></i>').animate({'opacity': 1}, 400);    
-                    });
-
-                    //set new text attribute
-                    ewa.attr("active-text", numberNeeded);
-                }
-            }else if(side == "left"){
-                if(atn == 1){
-
-                    //smallText
-                    $("#smallText").animate({'opacity': 0}, 400, function(){
-                        $("#smallText").html(slideTexts[numberOfSliderTexts].smallText).animate({'opacity': 1}, 400);    
-                    });
-
-                    //mainTitle
-                    $("#mainTitle").animate({'opacity': 0}, 400, function(){
-                        $("#mainTitle").html(slideTexts[numberOfSliderTexts].mainTitle).animate({'opacity': 1}, 400);    
-                    });
-
-                    //descText
-                    $("#descText").animate({'opacity': 0}, 400, function(){
-                        $("#descText").html(slideTexts[numberOfSliderTexts].descText).animate({'opacity': 1}, 400);    
-                    });
-
-                    //buttonText
-                    $("#buttonText").animate({'opacity': 0}, 400, function(){
-                        $("#buttonText").html(slideTexts[numberOfSliderTexts].buttonText+'<i class="icofont-simple-right ml-2  "></i>').animate({'opacity': 1}, 400);    
-                    });
-
-                    //set new text attribute
-                    ewa.attr("active-text", numberOfSliderTexts);
-                    }else{
-                    var numberNeeded = parseInt(atn) - 1;
-
-                    //smallText
-                    $("#smallText").animate({'opacity': 0}, 400, function(){
-                        $(this).html(slideTexts[numberNeeded].smallText).animate({'opacity': 1}, 400);    
-                    });
-
-                    //mainTitle
-                    $("#mainTitle").animate({'opacity': 0}, 400, function(){
-                        $("#mainTitle").html(slideTexts[numberNeeded].mainTitle).animate({'opacity': 1}, 400);    
-                    });
-
-                    //descText
-                    $("#descText").animate({'opacity': 0}, 400, function(){
-                        $("#descText").html(slideTexts[numberNeeded].descText).animate({'opacity': 1}, 400);    
-                    });
-
-                    //buttonText
-                    $("#buttonText").animate({'opacity': 0}, 400, function(){
-                        $("#buttonText").html(slideTexts[numberNeeded].buttonText+'<i class="icofont-simple-right ml-2  "></i>').animate({'opacity': 1}, 400);    
-                    });
-
-                    //set new text attribute
-                    ewa.attr("active-text", numberNeeded);
-                    }
-            }else{
-                console.log("Slider side not defined");
-            }
-        };
-
-        // SLIDER START
+    <!-- <script type="text/javascript">
         $(document).ready(function() {
-            $("#arrow-right").click(function(){
 
-                //get element with attributes
-                var elementWithAttrs = $(this).parent();
-
-                //side clicked variable
-                var x = "right";
-
-                //get slide and text numbers
-                var activeSlideNumber = $(this).parent().attr("active-slide");
-                var activeTextNumber = $(this).parent().attr("active-text");
-
-
-                if(activeSlideNumber == <?php echo $filecount ?>){
-                    $(".banner").css('background', 'url("../images/slider/1.jpg")');
-                    $(this).parent().attr("active-slide", 1);
-                    textSwitch(activeTextNumber, x, elementWithAttrs);
-                } else {
-                    var activeSlide = parseInt(activeSlideNumber) + 1;
-                    $(".banner").css('background', 'url("../images/slider/'+activeSlide+'.jpg")');
-                    $(this).parent().attr("active-slide", activeSlide);
-                    textSwitch(activeTextNumber, x, elementWithAttrs);
-                }
-            });
-            $("#arrow-left").click(function(){
-
-                //get element with attributes
-                var elementWithAttrs = $(this).parent();
-
-                //side clicked variable
-                var x = "left";
-
-                //get slide and text numbers
-                var activeSlideNumber = $(this).parent().attr("active-slide");
-                var activeTextNumber = $(this).parent().attr("active-text");
-
-                if(activeSlideNumber == 1){
-                    $(".banner").css('background', 'url("../images/slider/'+<?php echo $filecount ?>+'.jpg")');
-                    $(this).parent().attr("active-slide", <?php echo $filecount ?>);
-                    textSwitch(activeTextNumber, x, elementWithAttrs);
-                } else {
-                    var activeSlide = parseInt(activeSlideNumber) - 1;
-                    $(".banner").css('background', 'url("../images/slider/'+activeSlide+'.jpg")');
-                    $(this).parent().attr("active-slide", activeSlide);
-                    textSwitch(activeTextNumber, x, elementWithAttrs);
-                }
-            });
         });
-        // SLIDER END
-
-    </script>
+    </script> -->
 @endsection
