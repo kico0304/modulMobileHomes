325fc6b367651b8ec242f634b8a155c85f61ba00
unknown <stojan.vranjes8@gmail.com>
2022-02-23 12:22:50 +0100

    views

--- resources/views/admin/options.blade.php
+++ resources/views/admin/options.blade.php
@@ -2,13 +2,28 @@
 
 @section('componentcss')
     <style>
+        input[type=checkbox] + label {
+            width: 60px;
+            margin-bottom: 2px;
+            text-align: center;
+            padding: 2px 5px;
+            border-radius: 4px;
+            color: #fff;
+        }
 
+        input[type=checkbox]:checked + label {
+            background-color: rgba(90, 120, 217, 1);
+        }
+
+        input[type=checkbox]:not(:checked) + label {
+            background-color: #888;
+        }
     </style>
 @endsection
 
 @section('content')
 
-    <button class="add_modal btn btn-success">Add new Option</button>
+    <button class="add_modal btn btn-success">Add new Actualities</button>
 
     @if(session('success'))
         <div class="alert alert-success">
@@ -18,37 +33,27 @@
 
     <div class="modal_add">
 
-        <form method="post" action="{{url('/admin/add_option')}}" enctype="multipart/form-data">
+        <form method="post" action="{{url('/admin/add_actualities')}}" enctype="multipart/form-data">
 
             @csrf
-            <h3>Add new Option</h3>
+            <h3>Add new Actualities</h3>
             <p>All values must be inserted</p>
             <div class="row">
-                <label for="option_name">Name :</label>
-                <input id="option_name" name="name" type="text" class="form-control">
+                <label for="name">Name :</label>
+                <input id="name" name="name" type="text" class="form-control">
             </div>
             <div class="row">
-                <label for="option_price">Price :</label>
-                <input id="option_price" name="price" type="text" class="form-control">
+                <label>Text :</label>
+                <textarea name="text" type="text" class="form-control"></textarea>
+            </div>
+            <div class="row" style="margin-top: 10px; margin-bottom: 10px;">
+                @foreach($language as $lang)
+                    <input name="lang_{{$lang->id}}" id="actualities_{{$lang->lang}}" type="checkbox" hidden>
+                    <label for="actualities_{{$lang->lang}}" style="margin-left: 3px;">{{strtoupper($lang->lang)}}</label>
+                @endforeach
             </div>
             <div class="row">
-                <label for="option_surface">Surface :</label>
-                <input id="option_surface" name="surface" type="text" class="form-control">
-            </div>
-            <div class="row">
-                <label class="label_lang" for="option_en">EN :</label>
-                <textarea name="en" type="text" class="form-control textarea_cls"></textarea>
-            </div>
-            <div class="row">
-                <label class="label_lang" for="option_de">DE :</label>
-                <textarea name="de" type="text" class="form-control textarea_cls"></textarea>
-            </div>
-            <div class="row">
-                <label for="photo_upload">Upload Images</label>
-                <input type="file" name="photos[]" class="form-control uploaded_photo" id="photo_upload" multiple="">
-            </div>
-            <div class="row">
-                <button type="submit" class="btn btn-success">Save Option</button>
+                <button type="submit" class="btn btn-success">Save Actualities</button>
                 <button class="close_new_modal btn btn-danger">Close</button>
             </div>
 
@@ -56,72 +61,52 @@
 
     </div>
 
-    <table id="option_table" class="display" style="width:100%">
+    <table id="actualities_table" class="display" style="width:100%">
         <thead>
         <tr>
             <th class="text-center">ID</th>
-            <th class="text-center">Image</th>
             <th class="text-center">Name</th>
-            <th class="text-center">Price</th>
-            <th class="text-center">Surface</th>
-            <th class="text-center">EN</th>
-            <th class="text-center">DE</th>
+            <th class="text-center">Text</th>
+            <th class="text-center">Language</th>
+            <th class="text-center">Date</th>
             <th class="text-center">Edit</th>
             <th class="text-center">Delete</th>
         </tr>
         </thead>
         <tbody>
-        @if(!$options->isEmpty())
-            @foreach ($options as $option)
-                <div class="modal_edit edit_modal_{{$option->id}}">
+        @if(!$actualities->isEmpty())
+            @foreach ($actualities as $actualitie)
 
-                    <form method="post" action="{{url('/admin/edit_option')}}" enctype="multipart/form-data">
+                <div class="modal_edit edit_modal_{{$actualitie->id}}">
+
+                    <form method="post" action="{{url('/admin/edit_actualities')}}" enctype="multipart/form-data">
 
                         @csrf
-                        <h3>Edit Option</h3>
+                        <h3>Edit Actualities</h3>
                         <p>All values must be inserted</p>
-                        <input name="option_id" type="hidden" value="{{$option->id}}">
+                        <input name="actualities_id" type="hidden" value="{{$actualitie->id}}">
                         <div class="row">
                             <label>Name :</label>
-                            <input name="name" type="text" class="form-control" value="{{$option->name}}">
+                            <input name="name" type="text" class="form-control" value="{{$actualitie->name}}">
                         </div>
                         <div class="row">
-                            <label>Price :</label>
-                            <input name="price" type="number" step="any" class="form-control" value="{{$option->price}}">
-                        </div>
-                        <div class="row">
-                            <label for="option_surface">Surface :</label>
-                            <input id="option_surface" name="surface" type="text" class="form-control">
-                        </div>
-                        <div class="row">
-                            <label class="label_lang">EN :</label>
-                            <textarea name="en" type="text" class="form-control textarea_cls">{{$option->en}}</textarea>
-                        </div>
-                        <div class="row">
-                            <label class="label_lang">DE :</label>
-                            <textarea name="de" type="text" class="form-control textarea_cls">{{$option->de}}</textarea>
-                        </div>
-                        <div class="row">
-                            <label>Upload Images</label>
-                            <input type="file" name="photos[]" class="form-control uploaded_photo" multiple="">
+                            <label for="text">Text :</label>
+                            <input id="text" name="text" type="text" class="form-control" value="{{$actualitie->text}}">
                         </div>
 
-                        @if(!$option->option_images->isEmpty())
-                            <div class="row">
-                                @foreach($option->option_images as $image)
-                                    <div class="col-md-4 edit_photo_{{$image->id}}">
-                                        <div class="edit_photos">
-                                            <img class="edit_img" alt="edit_image" src="{{asset('images/options/option_'.$option->id.'/'.$image->name)}}">
-                                            <br>
-                                            <button class="btn btn-danger btn-sm delete_photo" data-name="{{$image->name}}" data-id="{{$option->id}}" data-photoid="{{$image->id}}">Delete</button>
-                                        </div>
-                                    </div>
+                        <div class="row" style="margin-top: 10px; margin-bottom: 10px;">
+                            @foreach($language as $lang)
+                                @php $check = ''; @endphp
+                                @foreach($actualitie->actualities_lang as $lang_ac)
+                                    @if($lang->lang == $lang_ac->lang) @php $check = 'checked'; @endphp @endif
                                 @endforeach
-                            </div>
-                        @endif
+                                <input name="lang_{{$lang->id}}" id="actualities_edit_{{$lang->lang}}" type="checkbox" hidden {{$check}}>
+                                <label for="actualities_edit_{{$lang->lang}}" style="margin-left: 3px;">{{strtoupper($lang->lang)}}</label>
+                            @endforeach
+                        </div>
 
                         <div class="row">
-                            <button type="submit" class="btn btn-success">Save Option Changes</button>
+                            <button type="submit" class="btn btn-success">Save Part Changes</button>
                             <button class="close_edit_modal btn btn-danger">Close</button>
                         </div>
 
@@ -129,23 +114,21 @@
 
                 </div>
                 <tr>
-                    <td>{{$option->id}}</td>
-                    <td>@if(!$option->option_images->isEmpty())<img class="img_table" alt="image" src="{{asset('images/options/option_'.$option->id.'/'.$option->option_images[0]->name)}}">@else - @endif</td>
-                    <td>{{$option->name}}</td>
-                    <td>{{$option->price}}</td>
-                    <td>{{$option->surface}}</td>
-                    <td>{{$option->en}}</td>
-                    <td>{{$option->de}}</td>
+                    <td>{{$actualitie->id}}</td>
+                    <td>{{$actualitie->name}}</td>
+                    <td>{{$actualitie->text}}</td>
+                    <td>@foreach($actualitie->actualities_lang as $lang) <span style="border: 1px solid green; padding: 5px;">{{$lang->lang}}</span> @endforeach</td>
+                    <td>{{$actualitie->created_at}}</td>
                     <td>
-                        <button class="edit_modal btn btn-primary" data-id="{{$option->id}}">Edit</button>
+                        <button class="edit_modal btn btn-primary" data-id="{{$actualitie->id}}">Edit</button>
                     </td>
                     <td>
-                        <button class="delete_option btn btn-danger" data-id="{{$option->id}}">Delete</button>
+                        <button class="delete_actualities btn btn-danger" data-id="{{$actualitie->id}}">Delete</button>
                     </td>
                 </tr>
             @endforeach
         @else
-            <h1>No options!</h1>
+            <h1>No actualities!</h1>
         @endif
         </tbody>
     </table>
@@ -156,7 +139,7 @@
     <script type="text/javascript">
         $(document).ready(function() {
             //datatables
-            $('#option_table').DataTable({
+            $('#actualities_table').DataTable({
                 "pageLength": 10,
                 "columnDefs": [ {
                     "className": "dt-center",
@@ -164,48 +147,21 @@
                 } ]
             });
 
-            //delete option
-            $('.delete_option').click(function () {
+            //delete actualities
+            $('.delete_actualities').click(function () {
 
-                if(!confirm('Are you sure you want to delete option?')){
+                if(!confirm('Are you sure you want to delete actualities?')){
                     return false;
                 }
 
                 let id = $(this).data('id');
                 $.ajax({
-                    url: '{{ url('/admin/delete_option') }}',
+                    url: '{{ url('/admin/delete_actualities') }}',
                     headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
                     data: {'id': id},
                     type: 'post',
                     success: function (ret) {
                         location.reload();
-                    },
-                    error: function (err) {
-                        alert("Error");
-                    }
-                })
-            });
-
-            //edit option delete photo
-            $('.delete_photo').click(function (e) {
-
-                e.preventDefault();
-
-                if(!confirm('Are you sure you want to delete option photo?')){
-                    return false;
-                }
-
-                let image_name = $(this).data('name');
-                let id = $(this).data('id');
-                let photo_id = $(this).data('photoid');
-
-                $.ajax({
-                    url: '{{ url('/admin/delete_option_photo') }}',
-                    headers: {'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')},
-                    data: {'image_name': image_name, 'id': id, 'photo_id': photo_id},
-                    type: 'post',
-                    success: function (ret) {
-                        $('.edit_photo_'+ret.image_id).hide();
                     },
                     error: function (err) {
                         alert("Error");
