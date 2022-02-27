c6f83ea688237e96da8a681146e79b5f446f4d3b
unknown <stojan.vranjes8@gmail.com>
2022-02-27 21:04:25 +0100

    actu

--- app/Http/Middleware/LangSubdomain.php
+++ app/Http/Middleware/LangSubdomain.php
@@ -26,12 +26,14 @@ class LangSubdomain
             $country_code = strtolower($get_country->countryCode);
         }
 
-        $languages = Language::all()->toArray();
+        $languages = Language::pluck('lang')->toArray();
 
-        if (in_array($subdomain, $languages)) {
+        if($country_code != ''){
+            if(in_array($country_code, $languages)){
+                App::setLocale($country_code);
+            }
+        }else if (in_array($subdomain, $languages)) {
             App::setLocale($subdomain);
-        }elseif(in_array($country_code, $languages)){
-            App::setLocale($country_code);
         }
 
         return $next($request);
