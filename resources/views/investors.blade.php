b83fdb8ad4b4fcf0a4839356c36bf789836b35f5
kico0304 <kico_jajcanin@hotmail.com>
2022-03-16 14:39:44 +0100

    investors done

--- resources/views/investors.blade.php
+++ resources/views/investors.blade.php
@@ -105,14 +105,14 @@
                 <form id="calc-form" class="calc-form" method="post" action="calc.php">
                     <div class="row centered">
                         <div class="col-lg-6 col-md-6">
-                            <button>Kalkulacija</button>
+                            <div id="calculationButton" class="btn btn-main btn-round-full btn-active">Kalkulacija</div>
                         </div>
                         <div class="col-lg-6 col-md-6">
-                            <button>Kontakt</button>
+                            <div id="sendCalculationButton" class="btn btn-main btn-round-full btn-inactive">Kontakt</div>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-lg-12 col-md-6 margined10">
+                    <div class="row" id="calculationRow">
+                        <div class="col-lg-12 col-md-12 margined10">
                             <div class="form-group">
                                 <label>Cena zemljišta (€)</label>
                                 <input name="landPrice" id="landPrice" type="number" class="form-control" placeholder="0 - posedujete zemljište">
@@ -151,13 +151,13 @@
                                 <img src="images/veca-soba_.png" />
                             </div>
                         </div>
-                        <div class="col-lg-12 col-md-6 margined10">
+                        <div class="col-lg-12 col-md-12 margined10">
                             <h3 class="myMainColorForTexts">Ukupan broj soba za izdavanje: <span id="ukupnoSoba">0</span></h3>
                         </div>
-                        <div class="col-lg-12 col-md-6 margined10">
+                        <div class="col-lg-12 col-md-12 margined10">
                             <h3 class="myMainColorForTexts">Ukupna investicija bez PDV-a: <span id="ukupnaInvesticija">0.00 €</span></h3>
                         </div>
-                        <div class="col-lg-12 col-md-6 margined10" style="margin-top: 50px!important">
+                        <div class="col-lg-12 col-md-12 margined10" style="margin-top: 50px!important">
                             <h4 class="myMainColorForTexts">Kreditni kalkulator</h4>
                             <div class="divider mx-auto my-4" style="margin: 0 0 0 0px!important"></div>
                         </div>
@@ -179,7 +179,7 @@
                                 <input name="totalMinus" id="totalMinus" type="number" class="form-control" placeholder="" disabled="true">
                             </div>
                         </div>
-                        <div class="col-lg-12 col-md-6 margined10">
+                        <div class="col-lg-12 col-md-12 margined10">
                             <h4 class="myMainColorForTexts">Prihodi</h4>
                             <div class="divider mx-auto my-4" style="margin: 0 0 0 0px!important"></div>
                         </div>
@@ -199,6 +199,90 @@
                             <div class="form-group">
                                 <label>Ukupni mesečni prihod (€)</label>
                                 <input name="totalPlus" id="totalPlus" type="number" class="form-control" placeholder="" disabled="true">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row" id="contactRow">
+                        <div class="col-lg-12 col-md-6">
+                            <h3 class="margined2030">Detalji Vaše kalkulacije:</h3>
+                        </div>
+                        <div class="col-lg-4 col-md-6">
+                            <p>Cena zemljišta: <span id="landPrice_">0 €</span></p>
+                        </div>
+                        <div class="col-lg-4 col-md-6">
+                            <p>Modul A (Garsonjera): <span id="modelAnumber_">0</span></p>
+                        </div>
+                        <div class="col-lg-4 col-md-6">
+                            <p>Modul B (Jednosoban stan): <span id="modelBnumber_">0</span></p>
+                        </div>
+                        <div class="col-lg-4 col-md-6">
+                            <p>Soba s kupatilom (D2): <span id="modelDnumber_">0</span></p>
+                        </div>
+                        <div class="col-lg-4 col-md-6">
+                            <p>Ukupan broj soba za izdavanje: <span id="finalBrojSoba_">0</span></p>
+                        </div>
+                        <div class="col-lg-4 col-md-6">
+                            <p>Ukupna investicija bez PDV-a: <span id="totalInvestion_">0 €</span></p>
+                        </div>
+                        <div class="col-lg-4 col-md-6">
+                            <p>Broj godina kredita: <span id="numberOfYears_"></span></p>
+                        </div>
+                        <div class="col-lg-4 col-md-6">
+                            <p>Godišnja kamatna stopa: <span id="loanInterest_"></span></p>
+                        </div>
+                        <div class="col-lg-4 col-md-6">
+                            <p>Ukupni mesečni rashod: <span id="loanPayment_"></span></p>
+                        </div>
+                        <div class="col-lg-4 col-md-6">
+                            <p>Cena najma sobe: <span id="dailyRentPrice_"></span></p>
+                        </div>
+                        <div class="col-lg-4 col-md-6">
+                            <p>Prosečna popunjenost: <span id="averageRent_"></span></p>
+                        </div>
+                        <div class="col-lg-4 col-md-6">
+                            <p>Ukupni mesečni prihod: <span id="totalIncome_"></span></p>
+                        </div>
+                        <div class="col-lg-12 col-md-6">
+                            <h3 class="margined2030">Kontaktirajte nas</h3>
+                        </div>
+                        <div class="col-lg-4 col-md-6">
+                            <div class="form-group">
+                                <label>Ime i prezime</label>
+                                <input name="name_" id="name_" type="text" class="form-control" placeholder="Ime i prezime">
+                                <span class="requiredField">*obavezno polje</span>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6">
+                            <div class="form-group">
+                                <label>E-mail</label>
+                                <input name="email_" id="email_" type="email" class="form-control" placeholder="E-mail adresa">
+                                <span class="requiredField">*obavezno polje</span>
+                            </div>
+                        </div>
+                        <div class="col-lg-4 col-md-6">
+                            <div class="form-group">
+                                <label>Telefon</label>
+                                <input name="phone_" id="phone_" type="text" class="form-control" placeholder="Broj telefona">
+                                <span class="requiredField">*nije obavezno polje</span>
+                            </div>
+                        </div>
+                        <div class="col-lg-12 col-md-6">
+                            <div class="form-group">
+                                <label>Tema poruke</label>
+                                <input name="subject_" id="subject_" type="text" class="form-control" placeholder="Tema poruke">
+                                <span class="requiredField">*obavezno polje</span>
+                            </div>
+                        </div>
+                        <div class="col-lg-12 col-md-6">
+                            <div class="form-group">
+                                <label>Poruka</label>
+                                <textarea name="message_" id="message_" class="form-control" rows="8" placeholder="Poruka"></textarea>
+                                <span class="requiredField">*obavezno polje</span>
+                            </div>
+                        </div>
+                        <div class="col-lg-12 col-md-6">
+                            <div class="form-group">
+                                <input id="submitContact_" class="btn btn-main btn-round-full" name="submit_" type="submit" value="Pošalji" disabled="true"></input>
                             </div>
                         </div>
                     </div>
@@ -250,54 +334,53 @@
             let dailyRentPrice;
             let averageRent;
             let totalIncome;
+            let clickedButton;
 
             //function for room numbers
             $("#modelAnumber, #modelBnumber, #modelDnumber").change(function(){
                 if($("#modelAnumber").val()){
                     modelAnumber = $("#modelAnumber").val();
-                    //console.log(modelAnumber);
+                    $("#modelAnumber_").text(modelAnumber);
                 } else{
                     modelAnumber = 0;
                 }
                 if($("#modelBnumber").val()){
                     modelBnumber = $("#modelBnumber").val();
-                    //console.log(modelBnumber);
+                    $("#modelBnumber_").text(modelBnumber);
                 } else{
                     modelBnumber = 0;
                 }
                 if($("#modelDnumber").val()){
                     modelDnumber = $("#modelDnumber").val();
-                    //console.log(modelDnumber);
+                    $("#modelDnumber_").text(modelDnumber);
                 } else{
                     modelDnumber = 0;
                 }
                 finalBrojSoba = parseInt(modelAnumber) + parseInt(modelBnumber)*2 + parseInt(modelDnumber);
                 $("#ukupnoSoba").text(finalBrojSoba);
+                $("#finalBrojSoba_").text(finalBrojSoba);
             });
 
             //function for prices and total investion
             $("#landPrice, #modelAnumber, #modelBnumber, #modelDnumber").change(function(){
                 if($("#landPrice").val()){
                     landPrice = $("#landPrice").val();
-                    //console.log(landPrice);
+                    $("#landPrice_").text(landPrice+"€");
                 } else{
                     landPrice = 0;
                 }
                 if($("#modelAnumber").val()){
                     modelAnumber = $("#modelAnumber").val();
-                    //console.log(modelAnumber);
                 } else{
                     modelAnumber = 0;
                 }
                 if($("#modelBnumber").val()){
                     modelBnumber = $("#modelBnumber").val();
-                    //console.log(modelBnumber);
                 } else{
                     modelBnumber = 0;
                 }
                 if($("#modelDnumber").val()){
                     modelDnumber = $("#modelDnumber").val();
-                    //console.log(modelDnumber);
                 } else{
                     modelDnumber = 0;
                 }
@@ -308,6 +391,7 @@
                     parseInt(modelDnumber)*parseInt(modelDnumberPrice);
                 
                 $("#ukupnaInvesticija").text(totalInvestion.toFixed(2)+" €");
+                $("#totalInvestion_").text(totalInvestion.toFixed(2)+" €");
             });
 
             //function for loan calculation
@@ -315,13 +399,13 @@
                 if(totalInvestion != undefined){
                     if($("#numberOfYears").val()){
                         numberOfYears = $("#numberOfYears").val();
-                        //console.log(numberOfYears);
+                        $("#numberOfYears_").text(numberOfYears);
                     } else{
                         numberOfYears = 0;
                     }
                     if($("#yearlyInterest").val()){
                         yearlyInterest = $("#yearlyInterest").val();
-                        //console.log(yearlyInterest);
+                        $("#loanInterest_").text(yearlyInterest + "%");
                     } else{
                         yearlyInterest = 0;
                     }
@@ -333,17 +417,18 @@
                     loanPayment = (totalInvestion * monthlyInterest * Math.pow(1 + monthlyInterest, monthsNumber)) / (Math.pow(1 + monthlyInterest, monthsNumber) - 1);
                     //writing result to input
                     $("#totalMinus").val(loanPayment.toFixed(2));
+                    $("#loanPayment_").text(loanPayment.toFixed(2)+ " €");
                 }
                 if(finalBrojSoba != undefined){
                     if($("#dailyRentPrice").val()){
                         dailyRentPrice = $("#dailyRentPrice").val();
-                        //console.log(dailyRentPrice);
+                        $("#dailyRentPrice_").text(dailyRentPrice+ " €");
                     } else{
                         dailyRentPrice = 0;
                     }
                     if($("#averageRent").val()){
                         averageRent = $("#averageRent").val();
-                        //console.log(averageRent);
+                        $("#averageRent_").text(averageRent+ "%");
                     } else{
                         averageRent = 0;
                     }
@@ -352,6 +437,7 @@
                     totalIncome = finalBrojSoba * dailyRentPrice * ((averageRent * 30) / 100);
 
                     $("#totalPlus").val(totalIncome.toFixed(2));
+                    $("#totalIncome_").text(totalIncome.toFixed(2)+ " €");
                 }
             });
 
@@ -372,6 +458,32 @@
             $("#myFlipper").css("width", "90%");
 
         });
+
+        /* calc and contact switch */
+
+        $("#calculationButton, #sendCalculationButton").click(function(){
+            if($(this).hasClass("btn-inactive")){
+                $(".btn-active").removeClass("btn-active").addClass("btn-inactive");
+                $(this).removeClass("btn-inactive").addClass("btn-active");
+                clickedButton = $(this).attr("id");
+            }
+            if(clickedButton == "calculationButton"){
+                $("#calculationRow").css("display", "flex");
+                $("#contactRow").hide();
+            }else{
+                $("#contactRow").css("display", "flex");
+                $("#calculationRow").hide();
+            }
+        });
+
+        //function for loan calculation
+        $("#name_, #email_, #phone_, #subject_, #message_").change(function(){
+                if(($("#name_").val()) && ($("#email_").val()) && ($("#subject_").val()) && ($("#message_").val())){
+                    $("#submitContact_").attr("disabled", false);
+                } else {
+                    $("#submitContact_").attr("disabled", true);
+                }
+            });
 
     </script>
 
