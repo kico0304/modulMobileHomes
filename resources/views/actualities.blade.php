d0dcaed73b240acc92f258c8590c996c65aac80a
unknown <stojan.vranjes8@gmail.com>
2022-03-17 20:54:47 +0100

    actu

--- resources/views/actualities.blade.php
+++ resources/views/actualities.blade.php
@@ -84,6 +84,70 @@
                 </div>
             </div>
 
+
+            @if (isset($paginator) && $paginator->lastPage() > 1)
+
+                <ul class="pagination">
+
+                <?php
+                $interval = isset($interval) ? abs(intval($interval)) : 3 ;
+                $from = $paginator->currentPage() - $interval;
+                if($from < 1){
+                    $from = 1;
+                }
+
+                $to = $paginator->currentPage() + $interval;
+                if($to > $paginator->lastPage()){
+                    $to = $paginator->lastPage();
+                }
+                ?>
+
+                <!-- first/previous -->
+                    @if($paginator->currentPage() > 1)
+                        <li>
+                            <a class="page-numbers" href="{{ $paginator->url(1) }}" aria-label="First">
+                                <span aria-hidden="true"><i class="icofont-thin-double-left"></i></span>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a class="page-numbers" href="{{ $paginator->url($paginator->currentPage() - 1) }}" aria-label="Previous">
+                                <span aria-hidden="true">‹</span>
+                            </a>
+                        </li>
+                    @endif
+
+                <!-- links -->
+                    @for($i = $from; $i <= $to; $i++)
+                        <?php
+                        $isCurrentPage = $paginator->currentPage() == $i;
+                        ?>
+                        <li class="{{ $isCurrentPage ? 'active' : '' }}">
+                            <a class="page-numbers" href="{{ !$isCurrentPage ? $paginator->url($i) : '#' }}">
+                                {{ $i }}
+                            </a>
+                        </li>
+                    @endfor
+
+                <!-- next/last -->
+                    @if($paginator->currentPage() < $paginator->lastPage())
+                        <li>
+                            <a class="page-numbers" href="{{ $paginator->url($paginator->currentPage() + 1) }}" aria-label="Next">
+                                <span aria-hidden="true">›</span>
+                            </a>
+                        </li>
+
+                        <li>
+                            <a class="page-numbers" href="{{ $paginator->url($paginator->lastpage()) }}" aria-label="Last">
+                                <span aria-hidden="true"><i class="icofont-thin-double-right"></i></span>
+                            </a>
+                        </li>
+                    @endif
+
+                </ul>
+
+            @endif
+
             <div class="row mt-5">
                 <div class="col-lg-8">
                     <nav class="pagination py-2 d-inline-block">
