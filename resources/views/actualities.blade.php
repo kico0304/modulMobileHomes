31cedcf3eaf3db825f914cc49c65f8b116b4588d
kico0304 <kico_jajcanin@hotmail.com>
2022-03-17 17:24:05 +0100

    test 2

--- resources/views/actualities.blade.php
+++ resources/views/actualities.blade.php
@@ -14,14 +14,6 @@
 
 @section('content')
 
-    @foreach($actualities as $actualitie)
-        <p>Actualities name: {{$actualitie->name}}</p>
-        <p>Actualities text: {{$actualitie->text}}</p>
-        @foreach($actualitie->images as $image)
-            <img class="edit_img" alt="edit_image" src="{{asset('images/actualities/actualities_'.$actualitie->id.'/'.$image->name)}}">
-        @endforeach
-    @endforeach
-
     <!-- HEADER START -->
     @include('header')
     <!-- HEADER END -->
@@ -39,59 +31,31 @@
             </div>
         </div>
     </section>
-
+    
     <section class="section blog-wrap">
         <div class="container">
             <div class="row">
                 <div class="col-lg-8">
                     <div class="row">
+                    @foreach($actualities as $actualitie)
                         <div class="col-lg-12 col-md-12 mb-5">
                             <div class="blog-item">
                                 <div class="blog-thumb">
-                                    <img src="images/aktuelnosti/blog-1.jpg" alt="" class="img-fluid ">
+                                @foreach($actualitie->images as $image)
+                                <img src="{{asset('images/actualities/actualities_'.$actualitie->id.'/'.$image->name)}}" alt="" class="img-fluid">
+                                @endforeach
                                 </div>
                                 <div class="blog-item-content">
                                     <div class="blog-item-meta mb-3 mt-4">
                                         <span class="text-black text-capitalize mr-3"><i class="icofont-calendar mr-1"></i>25. April 2021</span>
                                     </div>
-                                    <h2 class="mt-3 mb-3"><a href="{{ route('singlearticle') }}">Postavljanje uzornog objekta</a></h2>
-                                    <p class="mb-4">Danas smo postavili objekat MMH21.M01 na raskršću ulica Patre i Kralja Petra I Oslobodioca. Objekat ćemo koristiti kao SHOWROOM i moći ćete ga obići svaki radni dan od 12-16 sati (skraćeno zbog godišnjih odmora) ili po dogovoru sa našim prodajnim agentima. Obilazak možete zakazati na broj telefona 065 95 95 95 ili popunite formular na linku. </p>
+                                    <h2 class="mt-3 mb-3"><a href="{{ route('singlearticle') }}">{{$actualitie->name}}</a></h2>
+                                    <p class="mb-4">{{$actualitie->text}}</p>
                                     <a href="{{ route('singlearticle') }}" class="btn btn-main btn-icon btn-round-full">Opširnije<i class="icofont-simple-right ml-2  "></i></a>
                                 </div>
                             </div>
-                        </div>
-
-                        <div class="col-lg-12 col-md-12 mb-5">
-                            <div class="blog-item">
-                                <div class="blog-thumb">
-                                    <img src="images/aktuelnosti/blog-1.jpg" alt="" class="img-fluid ">
-                                </div>
-                                <div class="blog-item-content">
-                                    <div class="blog-item-meta mb-3 mt-4">
-                                        <span class="text-black text-capitalize mr-3"><i class="icofont-calendar mr-1"></i>25. April 2021</span>
-                                    </div>
-                                    <h2 class="mt-3 mb-3"><a href="{{ route('singlearticle') }}">Postavljanje uzornog objekta</a></h2>
-                                    <p class="mb-4">Danas smo postavili objekat MMH21.M01 na raskršću ulica Patre i Kralja Petra I Oslobodioca. Objekat ćemo koristiti kao SHOWROOM i moći ćete ga obići svaki radni dan od 12-16 sati (skraćeno zbog godišnjih odmora) ili po dogovoru sa našim prodajnim agentima. Obilazak možete zakazati na broj telefona 065 95 95 95 ili popunite formular na linku. </p>
-                                    <a href="{{ route('singlearticle') }}" class="btn btn-main btn-icon btn-round-full">Opširnije<i class="icofont-simple-right ml-2  "></i></a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-lg-12 col-md-12 mb-5">
-                            <div class="blog-item">
-                                <div class="blog-thumb">
-                                    <img src="images/aktuelnosti/blog-1.jpg" alt="" class="img-fluid ">
-                                </div>
-                                <div class="blog-item-content">
-                                    <div class="blog-item-meta mb-3 mt-4">
-                                        <span class="text-black text-capitalize mr-3"><i class="icofont-calendar mr-1"></i>25. April 2021</span>
-                                    </div>
-                                    <h2 class="mt-3 mb-3"><a href="{{ route('singlearticle') }}">Postavljanje uzornog objekta</a></h2>
-                                    <p class="mb-4">Danas smo postavili objekat MMH21.M01 na raskršću ulica Patre i Kralja Petra I Oslobodioca. Objekat ćemo koristiti kao SHOWROOM i moći ćete ga obići svaki radni dan od 12-16 sati (skraćeno zbog godišnjih odmora) ili po dogovoru sa našim prodajnim agentima. Obilazak možete zakazati na broj telefona 065 95 95 95 ili popunite formular na linku.</p>
-                                    <a href="{{ route('singlearticle') }}" class="btn btn-main btn-icon btn-round-full">Opširnije<i class="icofont-simple-right ml-2  "></i></a>
-                                </div>
-                            </div>
-                        </div>
+                        </div>                        
+                    @endforeach
                     </div>
                 </div>
                 <div class="col-lg-4">
